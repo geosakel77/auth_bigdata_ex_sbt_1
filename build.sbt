b5894b9c78534124b762a42e01e14d6a561c8114
george <mai1428@uom.edu.gr>
2017-01-04 10:44:19 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "auth_bigdata_ex_sbt_1"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/WordsTfIdf.scala
@@ -0,0 +1,23 @@
+
+/**
+  * Created by george on 4/1/2017.
+  */
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+
+object WordsTfIdf {
+
+
+
+
+
+
+
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+
+}
